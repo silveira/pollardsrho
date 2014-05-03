4718d9ef3b180d6a38db9593d998ab10a1e3ad63
Silveira Neto <silveiraneto@gmail.com>
2014-05-03 18:23:53 -0400

    indentation. added a factoring algorithm to compare results

--- src/benchmark_pollards_rho.py
+++ src/benchmark_pollards_rho.py
@@ -12,17 +12,28 @@ def rho(n):
   d = 1
   while d==1:
     x = f(x,n)
-    y = f(f(y,n),n)
+    y = f(f(y,n),n) 
     d = gcd(abs(x-y),n)
-  
   if d==n:
     return False
   return d
 
+def prime_factors(n):
+  factors = set()
+  i = 2
+  while i*i <= n:
+    while (n % i) == 0:
+      factors.add(i)
+      n = n / i
+    i += 1
+  if n > 1:
+    factors.add(n)
+  return factors	
+
 if __name__ == '__main__':
-	import timeit
-	for i in xrange(2,100):
-		print i,
-		t = timeit.timeit("rho({})".format(i), setup="from __main__ import rho")
-		print t, "rho({})={}".format(i,rho(i))
+  import timeit 
+  for i in xrange(2,100):
+    print i,
+    t = timeit.timeit("rho({})".format(i), setup="from __main__ import rho")
+    print t, "rho({})={}".format(i,rho(i))
 
