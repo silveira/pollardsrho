defffbbd33308590f56bcb9267eb76dc6228e8ed
Silveira Neto <silveiraneto@gmail.com>
2014-05-03 19:57:40 -0400

    comparing 3 functions.

--- src/benchmark_pollards_rho.py
+++ src/benchmark_pollards_rho.py
@@ -1,12 +1,23 @@
+import random 
+
 def gcd(a, b):
   while b:
     a, b = b, a%b
   return a
 
-def f(x,n):
+def square_minus_one(x,n):
   return (x*x-1)%n
 
-def rho(n):
+def square_plus_one(x,n):
+  return (x*x+1)%n
+
+def power_3_minus_one(x,n):
+  return (x*x*x-1)%n
+
+def random_(x,n):
+  return random.randint(0,n)
+
+def rho(n, f):
   x = 2
   y = 2
   d = 1
@@ -28,12 +39,15 @@ def prime_factors(n):
     i += 1
   if n > 1:
     factors.add(n)
-  return factors	
+  return factors
 
 if __name__ == '__main__':
   import timeit 
   for i in xrange(2,100):
     print i,
-    t = timeit.timeit("rho({})".format(i), setup="from __main__ import rho")
-    print t, "rho({})={}".format(i,rho(i))
+    t1 = timeit.timeit("rho({}, square_minus_one)".format(i), setup="from __main__ import rho, square_minus_one")
+    t2 = timeit.timeit("rho({}, square_plus_one)".format(i), setup="from __main__ import rho, square_plus_one")
+    t3 = timeit.timeit("rho({}, power_3_minus_one)".format(i), setup="from __main__ import rho, power_3_minus_one")
+#    t4 = timeit.timeit("rho({}, random_)".format(i), setup="from __main__ import rho, random_")
+    print t1,t2,t3
 
